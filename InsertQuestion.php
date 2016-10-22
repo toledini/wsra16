a75dc266a825062c0c34dccd56e26679f1fadcd1
Gorka Toledo Pellejero <gtoledo001@ikasle.ehu.eus>
2016-10-22 15:48:13 +0200

    xml lanean

--- InsertQuestion.php
+++ InsertQuestion.php
@@ -80,6 +80,8 @@
 	<div><textarea rows="10" cols="40" name="galdera" placeholder="Zein da hipertestuen transferentziarako protokoloa?" ></textarea><br/></div>
       Erantzuna(*):
 	<div><textarea rows="10" cols="40" name="erantzuna" placeholder="HTTP" ></textarea><br/></div>
+	  Gaia(*):
+	<div><input type="text" title="gaia" name="gaia" placeholder="Web Sistemak" /><br/></div>
 	  Zailtasuna:
 	<div><select name="zailtasuna">
 		<option value=" "> </option>
@@ -97,8 +99,8 @@
 <?php
 	
 	session_start();
-	//$niremysql = new mysqli("localhost","root","","quiz");
-	$niremysql = new mysqli("mysql.hostinger.es","u980005360_tol","joantol","u980005360_quiz");
+	$niremysql = new mysqli("localhost","root","","quiz");
+	//$niremysql = new mysqli("mysql.hostinger.es","u980005360_tol","joantol","u980005360_quiz");
 	
 	if ($niremysql->connect_error) {
 		printf("Konexio errorea: " . $niremysql->connect_error);
@@ -108,48 +110,69 @@
 				$galdera= $_POST["galdera"];
 				$erantzuna= $_POST["erantzuna"];
 				$zailtasun= $_POST["zailtasuna"];
-				
+								
 	}
 	
-	
+	$gaia= $_POST["gaia"];
 	$eposta=$_COOKIE["ErabiltzaileLog"];
 	
 	if (isset($_POST['bidali'])) {
-		if($galdera!=""){
-			if($erantzuna!=""){
-				$balioa = "INSERT INTO galderak (eposta, galdera, erantzuna, zailtasuna) VALUES ('$eposta','$galdera','$erantzuna','$zailtasun')"; 
-				if (!$niremysql -> query($balioa)){
-					die("<p>Errorea gertatu da: ".$niremysql -> error ."</p>");
-				}else{
-					echo 'Galdera zuzen sartu da';
-				}
-				if(!$ident= $niremysql-> query("SELECT identifikazioa FROM konexioak WHERE eposta='$eposta' ORDER BY identifikazioa DESC LIMIT 1")){
-						die("<p>Errore bat gertatu da: ".$niremysql -> error."</p>");
-				}
-				$row = mysqli_fetch_array($ident);
-				$ident=intval($row['identifikazioa']);
-				$mota="galdera txertatu";
-				$ordua= Date('Y-m-d H:i:s');
-				if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-					$ip = $_SERVER['HTTP_CLIENT_IP'];
-				} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-					$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-				} else {
-					$ip = $_SERVER['REMOTE_ADDR'];
-				}
-				$balioa= "INSERT INTO ekintzak (konexioa, eposta, mota, ordua, ip) values ('$ident', '$eposta', '$mota','$ordua','$ip')";
-				if (!$niremysql -> query($balioa)){
-					die("<p>Errore bat gertatu da: ".$niremysql -> error."</p>");
-				}
-			}else{
-				echo 'Erantzuna hutsa dago';
-			}
+		if($galdera!="" && filter_var($galdera,FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/([A-Z]+[a-z]*[ ]|[0-9]+)([A-z]|[a-z]+[ ]*|[-*+\/]?[0-9]+[ ]*)*\?/"))) === false){
+			echo("Galderaren formatua ez da egokia. <br/>");
+		}else if($galdera==""){
+			echo("Galdera hutsa dago. <br/>");
+		}else if($erantzuna!="" && filter_var($erantzuna,FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^([A-Z]+[ ]*|[a-z]+[ ]*|[0-9]+[ ]*)+$/"))) === false){
+			echo("Erantzunaren formatua ez da egokia. <br/>");
+		}else if($erantzuna==""){
+			echo("Erantzuna hutsa dago. <br/>");
+		}else if($gaia==""){
+			echo("Gaia hutsa dago. <br/>");
 		}else{
-			echo 'Galdera hutsa dago';
+			$balioa = "INSERT INTO galderak (eposta, galdera, erantzuna,gaia ,zailtasuna) VALUES ('$eposta','$galdera','$erantzuna','$gaia','$zailtasun')"; 
+			if (!$niremysql -> query($balioa)){
+				die("<p>Errorea gertatu da: ".$niremysql -> error ."</p>");
+			}else{
+				echo 'Galdera zuzen sartu da ';
+				echo "<br>";
+			}
+			if(!$ident= $niremysql-> query("SELECT identifikazioa FROM konexioak WHERE eposta='$eposta' ORDER BY identifikazioa DESC LIMIT 1")){
+				die("<p>Errore bat gertatu da: ".$niremysql -> error."</p>");
+			}
+			$row = mysqli_fetch_array($ident);
+			$ident=intval($row['identifikazioa']);
+			$mota="galdera txertatu";
+			$ordua= Date('Y-m-d H:i:s');
+			if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+				$ip = $_SERVER['HTTP_CLIENT_IP'];
+			} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+				$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+			} else {
+				$ip = $_SERVER['REMOTE_ADDR'];
+			}
+			$balioa= "INSERT INTO ekintzak (konexioa, eposta, mota, ordua, ip) values ('$ident', '$eposta', '$mota','$ordua','$ip')";
+			if (!$niremysql -> query($balioa)){
+				die("<p>Errore bat gertatu da: ".$niremysql -> error."</p>");
+			}else{
+			
+				$fitxategi= simplexml_load_file('galderak.xml');
+				$item= $fitxategi->addChild('assessmentItem');
+				$item->addAttribute('complexity', $zailtasun);
+				//adibideari jarraituz mikologiari buruzko galderak txertatuko ditugu
+				$item->addAttribute('subject', $gaia);
+				$body=$item->addChild('itemBody');
+				$body->addChild('p', $galdera);
+				$response=$item->addChild('correctResponse');
+				$response->addChild('value', $erantzuna);
+				if($fitxategi->asXML('galderak.xml') == 1){
+					echo "Ondo txertatu da XML fitxategian.";
+				}else{
+					echo "Gaizki txertatu da XML fitxategian.";
+				}
+				
+		    }
 		}
-	}else{
-		echo 'Sartu datuak eta sakatu Bidali';
-	}
+	}	
+
 
 ?>
 
