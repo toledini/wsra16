e4f3f5811c58763ad6583e3c8cf8a40fb6feb1bf
Gorka Toledo Pellejero <gtoledo001@ikasle.ehu.eus>
2016-11-05 13:58:45 +0100

    lab7 lanean

---
+++ RegistrationCheck.php
@@ -0,0 +1,16 @@
+<?php
+	require_once('lib/nusoap.php');
+	require_once('lib/class.wsdlcache.php');
+	$bezero=new nusoap_client('http://wsjiparsar.esy.es/webZerbitzuak/egiaztatuMatrikula.php?wsdl',false);
+	
+	$eposta = $_POST["eposta"];
+	if(isset($_POST["eposta"])){
+		$matrikula=$bezero->call('egiaztatuE',array('x'=>$_POST["eposta"]));
+		if($matrikula=="EZ"){
+			echo "EZ";
+		}else{
+			echo "BAI";
+		}
+	}
+
+?>
