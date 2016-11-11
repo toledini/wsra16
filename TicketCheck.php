f115cbee5be7f2d172a03290a2f2053665da3b13
Gorka Toledo Pellejero <gtoledo001@ikasle.ehu.eus>
2016-11-11 16:44:01 +0100

    hostingerrera igotzeko prest

--- TicketCheck.php
+++ TicketCheck.php
@@ -1,8 +1,8 @@
 <?php 
 	require_once('lib/nusoap.php');
 	require_once('lib/class.wsdlcache.php');
-	//$bezero=new nusoap_client('http://websistemak2016.esy.es/myquiz/ValidTicket.php?wsdl',false);
-	$bezero=new nusoap_client('http://localhost:1234/wsra16/myquiz/ValidTicket.php?wsdl',false);
+	$bezero=new nusoap_client('http://websistemak2016.esy.es/myquiz/ValidTicket.php?wsdl',false);
+	//$bezero=new nusoap_client('http://localhost:1234/wsra16/myquiz/ValidTicket.php?wsdl',false);
 	
 	if(isset($_POST['tiketa'])){
 		$tiketa = $bezero->call('ValidTicket',array('x'=>$_POST['tiketa']));
